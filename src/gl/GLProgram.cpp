68bdc0150feab7ae8438f97ab0c5595548e31693
chrisvm <christian.velez5@upr.edu>
2016-02-07 23:48:45 -0400

    program class for gl program abstraction

--- src/gl/GLProgram.cpp
+++ src/gl/GLProgram.cpp
@@ -3,3 +3,60 @@
 //
 
 #include "GLProgram.h"
+
+namespace GL {
+    Program::Program() {
+        _program = glCreateProgram();
+    }
+
+    void Program::attachShader(Shader *shader) {
+        attachShader(shader->location(), shader);
+    }
+
+    void Program::attachShader(std::string shaderName, Shader *shader) {
+        ShaderNode t;
+        t.name = shaderName;
+        t.shader = shader;
+        shader->attachTo(_program);
+        _shaders.push_back(t);
+    }
+
+    void Program::link() {
+        glLinkProgram(_program);
+    }
+
+    void Program::use() {
+        glUseProgram(_program);
+    }
+
+    GLuint Program::id() {
+        return _program;
+    }
+
+    GLint Program::uniformLocation(std::string location) {
+        return glGetUniformLocation(_program, location.c_str());
+    }
+
+    GLint Program::attribLocation(std::string location) {
+        return glGetAttribLocation(_program, location.c_str());
+    }
+
+    void Program::setVertexAttrib(std::string location, GLuint size, GLenum format, GLuint stride, GLuint index) {
+        GLint attrib = attribLocation(location);
+        GLint typeSize = getTypeSize(format);
+        stride = stride * typeSize;
+        index = index * typeSize;
+        glEnableVertexAttribArray(attrib);
+        glVertexAttribPointer(attrib, size, format, GL_FALSE, stride, (void*) (long)index);
+    }
+
+    GLuint Program::getTypeSize(GLenum type) {
+        if (type == GL_FLOAT)
+            return sizeof(float);
+        if (type == GL_INT)
+            return sizeof(int);
+        if (type == GL_DOUBLE)
+            return sizeof(double);
+        return sizeof(int);
+    }
+}
