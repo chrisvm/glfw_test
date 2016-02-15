46104c134bdc43f2ee29584a018d49350b707cb6
chrisvm <christian.velez5@upr.edu>
2016-02-15 12:39:51 -0400

    tree creation test

--- src/engine/drivers/tree_driver.cpp
+++ src/engine/drivers/tree_driver.cpp
@@ -1,6 +1,6 @@
 #include <string>
 #include <iostream>
-#include "Tree.h"
+#include "../Tree.h"
 
 int main() {
     std::string path = "../../";
@@ -8,6 +8,10 @@ int main() {
 
     Engine::Tree* tree = new Engine::Tree();
     tree->fromPath(path);
+
+    for (int x = 0; x < tree->count(); ++x) {
+        std::cout << x << " -> " << tree->getNode(x)->print() << std::endl;
+    }
 
     return 0;
 }
