bd663c894bb52df8a700023f90b0294f76d2797f
chrisvm <christian.velez5@upr.edu>
2016-02-21 22:16:13 -0400

    refactored to class

---
+++ src/engine/ObjFile.h
@@ -0,0 +1,14 @@
+//
+// Created by Christian Velez on 2/21/16.
+//
+
+#ifndef GLFW_TEST_OBJFILE_H
+#define GLFW_TEST_OBJFILE_H
+
+
+class ObjFile {
+
+};
+
+
+#endif //GLFW_TEST_OBJFILE_H
--- src/game_object/load_obj.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Christian Velez on 2/16/16.
-//
-
-#include "load_obj.h"
